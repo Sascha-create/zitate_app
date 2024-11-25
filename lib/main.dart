f90e272d0dd6373875af60f8a212a0382a3366b6
Sascha Ziegler <sascha8398@googlemail.com>
2024-11-25 14:52:48 +0100

    simple Screen with getQuote from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,73 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+const quoteUri = "https://api.api-ninjas.com/v1/quotes";
+
+Future<String> getDataFromApi(String uri) async {
+  final response = await http.get(Uri.parse(uri),
+      headers: {'X-Api-Key': 'yPN0DFDi++wyGgZeLqVhJA==ldxttGjxXwtcprbP'});
+
+  return response.body;
+}
+
+Future<String> getQuote() async {
+  final jsonString = await getDataFromApi(quoteUri);
+
+  final jsonObject = jsonDecode(jsonString);
+
+  final String quote = jsonObject[0]["quote"];
+
+  return quote;
+}
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String quote = "Noch kein Zitat geladen";
+
+  void getNewQuote() async {
+    quote = await getQuote();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple.shade800,
+          title: const Text("Zitate App"),
+          titleTextStyle: const TextStyle(
+              color: Colors.white, fontSize: 28, fontWeight: FontWeight.bold),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 240, child: Text(quote)),
+                const SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      getNewQuote();
+                    },
+                    child: const Text("Nächstes Zitat"))
+              ],
+            ),
+          ),
         ),
       ),
     );
